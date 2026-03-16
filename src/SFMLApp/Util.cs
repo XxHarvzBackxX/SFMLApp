c25a842ca25896d660ee75d434ecf9d17d736b98
Harvey Wells <Stampylongharv@gmail.com>
2026-03-16 11:36:29 +0000

    util

--- src/SFMLApp/Util.cs
+++ src/SFMLApp/Util.cs
@@ -37,7 +37,7 @@ internal class Util
 
         var normal = new Vector2f(-unit.Y, unit.X) * (width / 2f);
 
-        var quad = new VertexArray(PrimitiveType.Quads, 4);
+        VertexArray quad = new(PrimitiveType.Quads, 4);
 
         quad[0] = new Vertex(p1 - normal, colorStart);
         quad[1] = new Vertex(p2 - normal, colorEnd);
@@ -46,4 +46,49 @@ internal class Util
 
         return quad;
     }
+
+    public static Vector3f RotateY(Vector3f point, float theta)
+    {
+        float cosTheta = MathF.Cos(theta);
+        float sinTheta = MathF.Sin(theta);
+
+        float x = point.X * cosTheta - point.Z * sinTheta;
+        float z = point.X * sinTheta + point.Z * cosTheta;
+
+        return new(x, point.Y, z);
+    }
+
+    public static Vector3f RotateX(Vector3f point, float theta)
+    {
+        float cosTheta = MathF.Cos(theta);
+        float sinTheta = MathF.Sin(theta);
+
+        float y = point.Y * cosTheta - point.Z * sinTheta;
+        float z = point.Y * sinTheta + point.Z * cosTheta;
+
+        return new(point.X, y, z);
+    }
+
+    public static Vector3f RotateZ(Vector3f point, float theta)
+    {
+        float cosTheta = MathF.Cos(theta);
+        float sinTheta = MathF.Sin(theta);
+
+        float x = point.X * cosTheta - point.Y * sinTheta;
+        float y = point.X * sinTheta + point.Y * cosTheta;
+
+        return new(x, y, point.Z);
+    }
+
+    public static Vector3f MutateX(Vector3f point, float newX) => new(newX, point.Y, point.Z);
+
+    public static Vector3f MutateY(Vector3f point, float newY) => new(point.X, newY, point.Z);
+
+    public static Vector3f MutateZ(Vector3f point, float newZ) => new(point.X, point.Y, newZ);
+
+    public static Vector3f IncrementMutateX(Vector3f point, float incX) => new(point.X + incX, point.Y, point.Z);
+
+    public static Vector3f IncrementMutateY(Vector3f point, float incY) => new(point.X, point.Y + incY, point.Z);
+
+    public static Vector3f IncrementMutateZ(Vector3f point, float incZ) => new(point.X, point.Y, point.Z + incZ);
 }
