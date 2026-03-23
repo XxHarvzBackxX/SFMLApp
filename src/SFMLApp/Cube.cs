28f606c2ee75204857f994b256ef4c805c8215ea
Harvey Wells <Stampylongharv@gmail.com>
2026-03-23 12:17:52 +0000

    refactor to use faces; added shape base class; refactored some cube logic

--- src/SFMLApp/Cube.cs
+++ src/SFMLApp/Cube.cs
@@ -3,33 +3,21 @@ using SFML.System;
 
 namespace SFMLApp;
 
-public class Cube
+public class Cube : Shape
 {
-    public Vector3f Position { get; set; }
-    public float Scale { get; set; }
-    public Vector3f Rotation { get; set; }
-    private int[][] _vertices { get; set; } = [];
-
-    private static readonly int[][] _edges =
-    [
-        // back face edges
-        [0, 1], [1, 2],
-        [2, 3], [3, 0],
-        // front face edges
-        [4, 7], [7, 6],
-        [6, 5], [5, 4],
-        // back to front connecting edges
-        [0, 4], [1, 7],
-        [2, 6], [3, 5]
-    ];
-
+    public const float NearPlane = -0.1f;
     public Cube(float posX = 0f, float posY = 0f, float posZ = 0f, float rotX = 0f, float rotY = 0f, float rotZ = 0f, float scale = 1f)
     {
         Position = new Vector3f(posX, posY, posZ);
         Rotation = new Vector3f(rotX, rotY, rotZ);
         Scale = scale;
 
-        _vertices = [
+        _initModels();
+    }
+
+    private void _initModels()
+    {
+        _model = [
             [ -1, -1, -1 ], // 0-left, top, back
             [ 1, -1, -1 ], // 1-right, top, back
             [ 1, 1, -1 ], // 2-right, bottom, back
@@ -41,39 +29,31 @@ public class Cube
             [ 1, 1, 1 ], // 6-right, bottom, front
             [ 1, -1, 1 ], // 7-right, top, front
         ];
-    }
 
-    private const float NearPlane = -0.1f;
+        Faces = [
+            new Face([4, 5, 6, 7], this), // 0-front (near) face -- tlf, blf, brf, trf
+            new Face([1, 0, 3, 2], this), // 1-back (far) face -- trb, tlb, blb, brb
+            new Face([7, 1, 2, 6], this), // 2-right-hand face -- 
+            new Face([0, 4, 5, 3], this), // 3-left-hand face 
+            new Face([0, 1, 7, 4], this), // 4-top face
+            new Face([5, 6, 2, 3], this) // 5-bottom face
+        ];
+    }
 
     public void Draw()
     {
-        Vector2f[] projectedVertices = new Vector2f[_vertices.Length];
-        Vector3f[] worldVertices = new Vector3f[_vertices.Length];
+        Vector2f[] projectedVertices = new Vector2f[_model.Length];
+        Vector3f[] worldVertices = new Vector3f[_model.Length];
 
-        for (int vertexIndex = 0; vertexIndex < _vertices.Length; vertexIndex++)
+        for (int vertexIndex = 0; vertexIndex < _model.Length; vertexIndex++)
         {
-            int[] vertexCoords = _vertices[vertexIndex];
+            int[] vertexCoords = _model[vertexIndex];
             Vector3f modelSpacePoint = new Vector3f(vertexCoords[0], vertexCoords[1], vertexCoords[2]);
             Vector3f localSpacePoint = _toLocal(modelSpacePoint);
             worldVertices[vertexIndex] = _toWorld(localSpacePoint);
             projectedVertices[vertexIndex] = _toXY(worldVertices[vertexIndex]);
         }
 
-        foreach (int[] edgeIndices in _edges)
-        {
-            int startVertexIndex = edgeIndices[0];
-            int endVertexIndex = edgeIndices[1];
-
-            // skip this edge if either vertex is behind the 'near plane'
-            if (worldVertices[startVertexIndex].Z >= NearPlane || worldVertices[endVertexIndex].Z >= NearPlane)
-                continue;
-
-            // draw edges
-            Vector2f edgeStart = projectedVertices[startVertexIndex];
-            Vector2f edgeEnd = projectedVertices[endVertexIndex];
-            VertexArray edgeLine = Util.GradientLineFactory(edgeStart, edgeEnd, 2f, Color.Blue, Color.Cyan);
-            Program.Window!.Draw(edgeLine);
-        }
 
         for (int vertexIndex = 0; vertexIndex < projectedVertices.Length; vertexIndex++)
         {
@@ -86,35 +66,17 @@ public class Cube
             CircleShape vertexDot = Util.CircleFactory(3f, Color.Red, vertexScreenPos);
             Program.Window!.Draw(vertexDot);
         }
-    }
 
-    private Vector3f _toLocal(Vector3f point)
-    {
-        point *= Scale;
+        List<Color> colors = new List<Color>
+        {
+            Color.Red, Color.Blue, Color.Cyan, Color.White
+        };
 
-        Vector3f rY = Util.RotateY(point, Rotation.Y);
-        Vector3f rX = Util.RotateX(rY, Rotation.X);
-        Vector3f rZ = Util.RotateZ(rX, Rotation.Z);
 
-        return rZ;
-    }
-
-    private Vector3f _toWorld(Vector3f localPoint) => localPoint + Position;
-
-    private Vector2f _toXY(Vector3f worldPoint)
-    {
-        worldPoint.X /= worldPoint.Z;
-        worldPoint.Y /= worldPoint.Z;
-
-        Vector2f screenSize = new Vector2f(800, 600);
-
-        worldPoint.X *= screenSize.X;
-        worldPoint.Y *= screenSize.Y;
-
-        worldPoint.X += screenSize.X / 2;
-        worldPoint.Y += screenSize.Y / 2;
-
-        return new Vector2f(worldPoint.X, worldPoint.Y);
+        foreach (Face face in Faces)
+        {
+            face.Draw(worldVertices, projectedVertices);
+        }
     }
 
 }
---
+++ src/SFMLApp/Shape.cs
@@ -0,0 +1,46 @@
+﻿using SFML.System;
+
+namespace SFMLApp;
+
+public abstract class Shape
+{
+    public Vector3f Position { get; set; }
+    public float Scale { get; set; }
+    public Vector3f Rotation { get; set; }
+
+    public Face[] Faces { get; set; }
+
+    /// <summary>
+    /// Represents vertices
+    /// </summary>
+    protected int[][] _model { get; set; }
+
+    protected Vector3f _toLocal(Vector3f point)
+    {
+        point *= Scale;
+
+        Vector3f rY = Util.RotateY(point, Rotation.Y);
+        Vector3f rX = Util.RotateX(rY, Rotation.X);
+        Vector3f rZ = Util.RotateZ(rX, Rotation.Z);
+
+        return rZ;
+    }
+
+    protected Vector3f _toWorld(Vector3f localPoint) => localPoint + Position;
+
+    protected Vector2f _toXY(Vector3f worldPoint)
+    {
+        worldPoint.X /= worldPoint.Z;
+        worldPoint.Y /= worldPoint.Z;
+
+        Vector2f screenSize = new Vector2f(800, 600);
+
+        worldPoint.X *= screenSize.X;
+        worldPoint.Y *= screenSize.Y;
+
+        worldPoint.X += screenSize.X / 2;
+        worldPoint.Y += screenSize.Y / 2;
+
+        return new Vector2f(worldPoint.X, worldPoint.Y);
+    }
+}
